e983f146d3e16a71dee6be152ad763711c84e109
AalokMoliya <moliyaaalok1312@gmail.com>
2025-11-13 10:45:06 +0530

    Update timer_manager.h

--- include/timer_manager.h
+++ include/timer_manager.h
@@ -1,6 +1,3 @@
-/**
- * @author Team Member 5 - Timer/Interrupt Management
- */
 
 #ifndef TIMER_MANAGER_H
 #define TIMER_MANAGER_H
@@ -9,30 +6,25 @@
 
 /* FUNCTION PROTOTYPES - TIMING UTILITIES*/
 
-/**
- *For General RTOS
- */
+
+ //For General RTOS
+ 
 uint32_t timer_calculate_slice_ticks(uint32_t time_slice_ms);
 
-/**
- *Start SysTick timer with given reload value (polling mode)
- * @param reload_ticks Reload value for SysTick counter
- * This is for CortexM3
- */
+//Start SysTick timer with given reload value (polling mode)
+//This is for CortexM3
+ 
 void timer_start_slice(uint32_t reload_ticks);
 
-/**N
- * Stop SysTick timer
- * This is for CortexM3
- */
+// Stop SysTick timer
+// This is for CortexM3
+ 
 void timer_stop_slice(void);
 
-/**
- *Check if current time slice has expired
- * @return bool True if slice expired
- * This is for CortexM3
- */
+//Check if current time slice has expired
+// This is for CortexM3
+ 
 bool timer_slice_expired(void);
 
-#endif /* TIMER_MANAGER_H */
+#endif 
 
